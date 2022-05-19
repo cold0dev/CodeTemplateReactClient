8da5b5036c3186d3a9cc04830fe950c7d6d0ac57
Lukasz Kwiatkowski <lkwiatkowski@outlook.com>
2022-05-19 01:19:10 +0100

    GetData.js: created

--- src/GetData.js
+++ src/GetData.js
@@ -1,7 +1,7 @@
-async function postData(url = "", data = {},isJSONResponse) {
+async function getData(url = "",isJSONResponse) {
     // Default options are marked with *
     const response = await fetch(url, {
-      method: "POST", // *GET, POST, PUT, DELETE, etc.
+      method: "GET", // *GET, POST, PUT, DELETE, etc.
       mode: "cors", // no-cors, *cors, same-origin
       cache: "no-cache", // *default, no-cache, reload, force-cache, only-if-cached
       //credentials: "same-origin", // include, *same-origin, omit
@@ -11,10 +11,9 @@ async function postData(url = "", data = {},isJSONResponse) {
       },
       redirect: "follow", // manual, *follow, error
       referrerPolicy: "no-referrer", // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
-      body: JSON.stringify(data) // body data type must match "Content-Type" header
     });
     if(isJSONResponse)return response.json(); // parses JSON response into native JavaScript objects
     return response.text();
 }
 
-export default postData;
+export default getData;
